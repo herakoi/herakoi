a3a0a2dc02647fe54c25f409503b2afeee79a9a9
Luca Di Mascolo <lucadimascolo@users.noreply.github.com>
2022-07-26 22:47:17 +0200

    Add files via upload

--- sonic.py
+++ sonic.py
@@ -5,8 +5,21 @@ import cv2
 
 import mido
 import rtmidi
+import rtmidi.midiconstants as midiconst
 
 import time
+
+# ------------------------
+
+def encode(status,data1=None,data2=None,channel=None):
+  msg = [(status & 0xF0) | (channel-1 & 0xF)]
+  if data1 is not None:
+    msg.append(data1 & 0x7F)
+  
+    if data2 is not None:
+      msg.append(data2 & 0x7F)
+
+  return msg
 
 # ------------------------
 
@@ -31,13 +44,22 @@ musimage_h, musimage_w, _ = musimage.shape
 
 musimage_HSV = cv2.cvtColor(musimage, cv2.COLOR_BGR2HSV)
 musimage_hue = musimage_HSV[:,:,0].copy()
+musimage_sat = musimage_HSV[:,:,1].copy()
 musimage_bri = musimage_HSV[:,:,2].copy()
+
+musimage_hue_val = (musimage_hue.min(),musimage_hue.max())
+musimage_bri_val = (musimage_bri.min(),musimage_bri.max())
 
 cap = cv2.VideoCapture(0)
 
-lhbox = 10; lhcol = (0,255,0)
-rhbox = 10; rhcol = (0,255,255)
+lhbox = 50; lhcol = (0,255,0)
+rhbox = 50; rhcol = (0,255,255)
 
+toc = 0.01
+tic = time.time()
+
+lhold = [int(musimage_w/2),int(musimage_h/2),122]
+rhold = [int(musimage_w/2),int(musimage_h/2),122]
 while True:
   success, frame = cap.read()
   frame = cv2.flip(frame, 1)
@@ -46,6 +68,8 @@ while True:
   music = np.copy(musimage)
   results = hands.process(frameRGB)
   
+  lhfreq, lhvelo = 63, 0
+  rhfreq, rhvelo = 63, 0
   if results.multi_hand_landmarks:
     for hi, handLms in enumerate(results.multi_hand_landmarks):
 
@@ -80,21 +104,48 @@ while True:
                  np.abs(lm.z)]
         cv2.circle(music,(rhpos[0],rhpos[1]),np.clip(-int(lm.z*300),2,None),rhcol,-1)
 
-      freq = np.mean(musimage_hue[np.clip(lhpos[1]-lhbox//2,0,musimage_h-1):np.clip(lhpos[1]+lhbox//2,0,musimage_h-1),
-                                  np.clip(lhpos[0]-lhbox//2,0,musimage_2-1):np.clip(lhpos[0]+lhbox//2,0,musimage_2-1)])
-      freq = 0 if np.isnan(freq) else freq
-      freq = int(np.interp(freq,(0,255),(48,95)))
+      lhhue = np.mean(musimage_hue[np.clip(lhpos[1]-lhbox//2,0,musimage_h-1):np.clip(lhpos[1]+lhbox//2,0,musimage_h-1),
+                                   np.clip(lhpos[0]-lhbox//2,0,musimage_w-1):np.clip(lhpos[0]+lhbox//2,0,musimage_w-1)])
 
-      velo = np.mean(musimage_bri[np.clip(lhpos[1]-lhbox//2,0,musimage_h-1):np.clip(lhpos[1]+lhbox//2,0,musimage_h),
-                                  np.clip(lhpos[0]-lhbox//2,0,musimage_w-1):np.clip(lhpos[0]+lhbox//2,0,musimage_w)])
-      velo = 0 if np.isnan(velo) else velo
-      velo = int(np.interp(velo,(0,255),(0,127)))
+      lhsat = np.mean(musimage_sat[np.clip(lhpos[1]-lhbox//2,0,musimage_h-1):np.clip(lhpos[1]+lhbox//2,0,musimage_h-1),
+                                   np.clip(lhpos[0]-lhbox//2,0,musimage_w-1):np.clip(lhpos[0]+lhbox//2,0,musimage_w-1)])
 
-      note_on  = mido.Message('note_on',channel=9,note=freq,velocity=velo).bytes()
-      note_off = mido.Message('note_off',channel=9,note=freq,velocity=0).bytes()
+      lhbri = np.mean(musimage_bri[np.clip(lhpos[1]-lhbox//2,0,musimage_h-1):np.clip(lhpos[1]+lhbox//2,0,musimage_h),
+                                   np.clip(lhpos[0]-lhbox//2,0,musimage_w-1):np.clip(lhpos[0]+lhbox//2,0,musimage_w)])
 
-      midiout.send_message(note_on)
-      midiout.send_message(note_off)
+      rhhue = np.mean(musimage_hue[np.clip(rhpos[1]-rhbox//2,0,musimage_h-1):np.clip(rhpos[1]+rhbox//2,0,musimage_h-1),
+                                   np.clip(rhpos[0]-rhbox//2,0,musimage_w-1):np.clip(rhpos[0]+rhbox//2,0,musimage_w-1)])
+
+      rhsat = np.mean(musimage_sat[np.clip(rhpos[1]-rhbox//2,0,musimage_h-1):np.clip(rhpos[1]+rhbox//2,0,musimage_h-1),
+                                   np.clip(rhpos[0]-rhbox//2,0,musimage_w-1):np.clip(rhpos[0]+rhbox//2,0,musimage_w-1)])
+
+      rhbri = np.mean(musimage_bri[np.clip(rhpos[1]-rhbox//2,0,musimage_h-1):np.clip(rhpos[1]+rhbox//2,0,musimage_h),
+                                   np.clip(rhpos[0]-rhbox//2,0,musimage_w-1):np.clip(rhpos[0]+rhbox//2,0,musimage_w)])
+
+      lhfval = musimage_hue_val
+      lhvval = musimage_bri_val
+      lhfreq = lhhue; lhfreq = 0 if np.isnan(lhfreq) else lhfreq; lhfreq = int(np.interp(lhfreq,lhfval,(48,95)))
+      lhvelo = lhbri; lhvelo = 0 if np.isnan(lhvelo) else lhvelo; lhvelo = int(np.interp(lhvelo,lhvval,(0,127)))
+
+      rhfval = musimage_hue_val
+      rhvval = musimage_bri_val
+      rhfreq = rhhue; rhfreq = 0 if np.isnan(rhfreq) else rhfreq; rhfreq = int(np.interp(rhfreq,rhfval,(48,95)))
+      rhvelo = rhbri; rhvelo = 0 if np.isnan(rhvelo) else rhvelo; rhvelo = int(np.interp(rhvelo,rhvval,(0,127)))
+
+      if np.abs(time.time()-tic)>toc:
+        if np.hypot(lhpos[0]-lhold[0],lhpos[1]-lhold[1])>1.00E-03*np.hypot(musimage_w,musimage_h):
+          midiout.send_message(encode(midiconst.NOTE_ON,lhfreq,lhvelo,channel=8)); lhold = lhpos.copy()
+          midiout.send_message(encode(midiconst.NOTE_ON,rhfreq,rhvelo,channel=9)); rhold = rhpos.copy()
+
+        time.sleep(0.01)
+        midiout.send_message(encode(midiconst.NOTE_OFF,lhfreq,0,channel=8))
+        midiout.send_message(encode(midiconst.NOTE_OFF,lhfreq,0,channel=9))
+        tic = time.time()
+
+        lhold = lhpos.copy()
+    else:
+      midiout.send_message(encode(midiconst.NOTE_OFF,lhfreq,0,channel=8))
+      midiout.send_message(encode(midiconst.NOTE_OFF,lhfreq,0,channel=9))
 
   cv2.imshow('frame', frame)
   cv2.imshow('musimage', music)
